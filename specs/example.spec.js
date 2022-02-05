3e39921a13dbeff8beb90fb437aafb35bb971215
Irina Burnaby <kassiopeamaratik@gmail.com>
2022-02-04 23:10:40 -0500

    add environment variables support

--- specs/example.spec.js
+++ specs/example.spec.js
@@ -1,16 +1,17 @@
-import { expect } from 'chai'
+import {expect} from 'chai'
+// const expect1 = require('chai').expect
 
-describe ('Math functions', function() {
+describe('Math functions', function () {
   const a = 4
   const b = 5
   const c = 9
   const d = -1
 
-  it.skip('A + B = C', function() {
+  it.skip('A + B = C', function () {
     expect(a + b).to.eq(c)
   })
 
-    it.only('A - B = D', function () {
-      expect(a - b).to.eq(d)
+  it.only('A - B = D', function () {
+    expect(a - b).to.eq(d)
   })
 })
